e6a77e3cd832a2508f4b9f4f7cf4439b5e1625bc
234945 <cvetnovaarina@gmail.com>
2026-01-30 10:28:03 +0300

    осталос доработать сохранение в бд

--- WpfApp1/Pages/Page2.xaml.cs
+++ WpfApp1/Pages/Page2.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp1.Models;
 
 namespace WpfApp1.Pages
 {
@@ -24,6 +25,7 @@ namespace WpfApp1.Pages
         {
             InitializeComponent();
             List<Product> product = Core.Context.Product.ToList();
+            ProductListBox2.ItemsSource = Class1.basket;
         }
         private void ButtonGoForward2_Click(object sender, RoutedEventArgs e)
         {
--- WpfApp1/Pages/Page3.xaml.cs
+++ WpfApp1/Pages/Page3.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp1.Models;
 
 namespace WpfApp1.Pages
 {
@@ -23,10 +25,25 @@ namespace WpfApp1.Pages
         public Page3()
         {
             InitializeComponent();
+            List<Product> product = Core.Context.Product.ToList();
+            List<Order> order = Core.Context.Order.ToList();
+            List<OrderProduct> ordpro = Core.Context.OrderProduct.ToList();
+            ProductListBox3.ItemsSource = Class1.basket;
+            int c = 0;
+            foreach (Product pro in Class1.basket)
+                c += pro.price;
+            FullPrice.Text = c.ToString();
         }
         private void ButtonSafe_Click(object sender, RoutedEventArgs e)
         {
-
+            Order newOrder = new Order // создание нового пользователя
+            {
+                username = ,
+                mail = ,
+                address = 
+            };
+            Core.Context.Order.Add(newOrder);
+            Core.Context.SaveChanges();
         }
     }
 }
