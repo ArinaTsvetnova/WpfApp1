e6ecc75dc3aec968ff0fe6fc5c2899d4f31a3e02
234945 <cvetnovaarina@gmail.com>
2026-02-02 15:41:41 +0300

    оформлен лист бокс главной страницы  и ереход на вход в личный аккаунт

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -24,5 +24,8 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+        }
     }
 }
--- WpfApp1/Pages/Page1.xaml.cs
+++ WpfApp1/Pages/Page1.xaml.cs
@@ -20,9 +20,46 @@ namespace WpfApp1.Pages
     /// </summary>
     public partial class Page1 : Page
     {
+        public class Sortir
+        {
+            public string Name { get; set; }
+        }
         public Page1()
         {
             InitializeComponent();
+            List<Film> film = Core.Context.Film.ToList();
+            FilmListBox.ItemsSource = film;
+            List<Sortir> ssort = new List<Sortir>()
+            {
+                new Sortir
+                {
+                    Name = "-",
+                },
+                new Sortir
+                {
+                    Name = "По жанрам",
+                },
+                new Sortir
+                {
+                    Name = "По рейтингу",
+                }
+            };
+
+            Sort.ItemsSource = ssort;
+            Sort.DisplayMemberPath = "Name";
+            Sort.SelectedIndex = 0;
+        }
+
+        private void ButtonEntry_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Page2());
+            if (NavigationService.CanGoForward)
+            {
+                NavigationService.GoForward();
+            }
+        }
+        private void ButtonBuy_Click(object sender, RoutedEventArgs e)
+        {
 
         }
     }
