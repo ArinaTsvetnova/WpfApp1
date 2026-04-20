57a294db28e0852bd60a5b572647a3f8503d0d3d
234945 <cvetnovaarina@gmail.com>
2026-04-20 12:01:06 +0300

    страница оформления записи

--- WpfApp1/Pages/AppointmentListPageView.xaml.cs
+++ WpfApp1/Pages/AppointmentListPageView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace WpfApp1.Pages
 {
@@ -29,14 +30,40 @@ namespace WpfApp1.Pages
             _master = master;
 
             // Заполняем интерфейс данными
-            //TxtServiceName.Text = _service.Name;
-            //TxtMasterName.Text = _master.FullName;
-            //TxtPrice.Text = $"{_service.BasePrice} руб."; // Или цена из MasterServices
+            //ServiceName.Text = _service.Name;
+            //MasterName.Text = _master.FullName;
+            //Price.Text = $"{_service.BasePrice} руб."; // Или цена из MasterServices
 
             // Можно сразу подставить клиента, если он есть в сессии
             if (AppSession.CurrentUser != null)
             {
                 // Если нужно отображать данные клиента
+            }
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            //var newAppointment = new Appointments
+            //{
+            //    ClientId = AppSession.CurrentUser.Id,
+            //    MasterId = _master.Id,
+            //    ServiceTypeId = _service.Id,
+            //    AppointmentDateTime = DpDate.SelectedDate.Value + TpTime.SelectedTime.Value,
+            //    Status = "Scheduled",
+            //    Comment = TxtComment.Text
+            //};
+
+            //Core.Context.Appointments.Add(newAppointment);
+            //Core.Context.SaveChanges();
+
+            //MessageBox.Show("Вы успешно записаны!");
+        }
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new StartPageView());
+            if (NavigationService.CanGoForward)
+            {
+                NavigationService.GoForward();
             }
         }
     }
